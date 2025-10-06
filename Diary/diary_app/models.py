a89dc9b30a8f80d29745d90c960902972909c96a
HDAN <cdh2895924329@gmail.com>
2025-10-06 19:12:51 +0800

    添加星期

--- Diary/diary_app/models.py
+++ Diary/diary_app/models.py
@@ -14,9 +14,16 @@ class Diary(models.Model):
             (5, '星期五'),
             (6, '星期六'),
             (7, '星期日'),
-        )
+        ),
+        default=1  # 默认值，会在save方法中被覆盖
     )
 
+    def save(self, *args, **kwargs):
+        # 根据Data字段自动设置星期几
+        # weekday()返回0-6，对应星期一到星期日
+        # 我们需要将其转换为1-7
+        self.week = self.Data.weekday() + 1
+        super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'diary'
